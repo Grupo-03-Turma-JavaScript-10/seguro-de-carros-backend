02b9657fa3be2edc6f9d0d2c2eb9ad145239f162
Mamadou DIAGNE <diagne264@gmail.com>
2025-12-18 10:25:01 -0300

    feat: cria service de apolice com relacionamento cliente e veiculo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApoliceService } from './apolice/apolice.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ApoliceService],
 })
 export class AppModule {}
