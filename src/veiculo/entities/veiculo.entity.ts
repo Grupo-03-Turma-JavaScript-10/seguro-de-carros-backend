9d6c509f845c4ea3295138abc3117f8d4d0cf314
MarcosCRosa <marcosluiz4012@gmail.com>
2025-12-18 09:55:14 -0300

    feat: validação entidades

--- src/veiculo/entities/veiculo.entity.ts
+++ src/veiculo/entities/veiculo.entity.ts
@@ -1,19 +1,25 @@
-import { Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { IsNotEmpty, IsNumber } from 'class-validator';
 
 @Entity({ name: 'tb_veiculos'})
  export class Veiculo{
     @PrimaryGeneratedColumn()
     id:number;
 
+    @IsNotEmpty()
     @Column({length:255,nullable:false})
     placa:string;
-
+    
+    @IsNotEmpty()
     @Column({length:255,nullable:false})
     modelo:string;
-
+    
+    @IsNotEmpty()
     @Column({length:255,nullable:false})
     marca:string;
-
+    
+    @IsNumber()
+    @IsNotEmpty()
     @Column({type:'int',nullable:false})
     ano:number;
 
