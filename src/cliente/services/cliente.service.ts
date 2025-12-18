f6590d7632711ee8dcafab4e0dccfdd2cf0533e0
Mamadou DIAGNE <diagne264@gmail.com>
2025-12-18 13:43:39 -0300

    feat: estrutura base de cliente com controller

---
+++ src/cliente/services/cliente.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ClienteService {
+  create(cliente: Cliente) {
+    throw new Error('Method not implemented.');
+  }
+  findAll() {
+    throw new Error('Method not implemented.');
+  }
+  findById(id: number) {
+    throw new Error('Method not implemented.');
+  }
+  update(id: number, cliente: Cliente) {
+    throw new Error('Method not implemented.');
+  }
+  delete(id: number) {
+    throw new Error('Method not implemented.');
+  }
+}
