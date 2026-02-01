5066124b11ea65f7cccbcbcb18a5ec26a5704c7f
MarcosCRosa <marcosluiz4012@gmail.com>
2026-02-01 20:35:28 -0300

    feat: Criada rota getMe para trazer dados do usuario pelo id.

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,11 +1,12 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards, Request } from "@nestjs/common";
-import { ApiBody, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards, Request } from "@nestjs/common";
+import { ApiBody, ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 import { AuthService } from '../services/auth.service';
 import { ClientLogin } from '../entities/clientlogin.entity';
 import { LocalAuthGuard } from '../guard/local-auth.guard';
 import { ClienteResponse } from '../interfaces/cliente-response.interface';
 import { ClienteService } from '../../cliente/services/cliente.service';
 import { Cliente } from '../../cliente/entities/cliente.entity';
+import { JwtAuthGuard } from '../guard/jwt-auth.guard';
 
 @ApiTags('Autenticação')
 @Controller('/clientes')
@@ -27,5 +28,13 @@ export class AuthController {
   @ApiBody({ type: ClientLogin })
   async login(@Request() req: any): Promise<ClienteResponse> {
     return this.authservice.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/me')
+  @HttpCode(HttpStatus.OK)
+  @ApiBearerAuth()
+  async getMe(@Request() req: any): Promise<Cliente> {
+    return this.clienteService.findById(req.user.id);
   }
 }
